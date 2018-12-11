9b9a3381352c2a0868e053cd07132d87ce6031c4
Andrés <andresin@gmail.com>
2018-12-11 13:48:05 -0300

    Se agrega encodore para ORM

--- src/class/default-hl7/index.js
+++ src/class/default-hl7/index.js
@@ -1,10 +1,15 @@
 import ADT from './hl7/adt'
+import ORM from './hl7/orm'
 import SIU from './hl7/siu'
 
 export default class DefaultEncoder {
 
   getAdtEncoder(message) {
     return new ADT(message)
+  }
+
+  getOrmEncoder(message) {
+    return new ORM(message)
   }
 
   getSiuEncoder(message) {
